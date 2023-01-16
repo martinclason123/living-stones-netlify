64414fc2454d9a73e138897158deed18235c5e2c
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2023-01-16 13:12:47 -0500

    "typo fix"

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero/Hero";
 import React from "react";
 
 import Header from "../components/Header/Header";
-import { Container } from "./LayoutStyles";
+import { Container } from "./layoutStyles";
 
 export const Layout = ({ children }) => {
   return (
