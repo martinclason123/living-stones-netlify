809e7bc72a2990230f9c3fb5c1689a690bcaaade
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2023-07-24 20:14:49 -0400

    fixed map alignment

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,8 +1,8 @@
 import { Section } from "../../styles/GlobalComponents";
-import { Map, Address } from "./LocationStyles";
+import { LocationSection, Map, Address } from "./LocationStyles";
 const Location = () => {
   return (
-    <Section width="100%" id="location">
+    <LocationSection flex column width="100%" id="location">
       <Map
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2904.6227415329054!2d-85.55331042287185!3d43.28027877646906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8818e308742ea39d%3A0x1571453190c166ad!2s17322%20Cedar%20Springs%20Ave%20NE%2C%20Sand%20Lake%2C%20MI%2049343!5e0!3m2!1sen!2sus!4v1689265903330!5m2!1sen!2sus"
         allowfullscreen=""
@@ -13,7 +13,7 @@ const Location = () => {
       <Address target={"_blank"} href="https://goo.gl/maps/8od31JQ9CEokCk6r6">
         17322 Cedar Springs Ave, Sand Lake, MI 49343
       </Address>
-    </Section>
+    </LocationSection>
   );
 };
 
--- src/components/Location/LocationStyles.js
+++ src/components/Location/LocationStyles.js
@@ -1,5 +1,28 @@
 import { productionBrowserSourceMaps } from "next.config";
+import { Section } from "@/styles/GlobalComponents";
 import styled from "styled-components";
+
+export const LocationSection = styled(Section)`
+  display: flex;
+  flex-direction: ${(props) => (props.column ? "column" : "row")};
+  background: ${(props) =>
+    props.background ? `${props.background}` : "transparent"};
+  padding: ${(props) => (props.padding ? "32px 48px 0" : "0")};
+  margin: ${(props) => (props.margin ? props.margin : "0 auto")};
+  max-width: 1920px;
+  box-sizing: content-box;
+  position: ${(props) => (props.position ? props.position : "relative")};
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    font-size: 1.563vw;
+  }
+  @media ${(props) => props.theme.breakpoints.md} {
+    font-size: 0.521vw;
+  }
+  @media ${(props) => props.theme.breakpoints.max} {
+    font-size: 10px;
+  }
+`;
 
 export const Map = styled.iframe`
   width: 182.2em;
