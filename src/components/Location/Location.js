34911d17ce6501136811a3adb73b9287be7ca60b
martinclason123 <39966439+martinclason123@users.noreply.github.com>
2023-11-10 13:53:47 -0500

    address update

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -4,14 +4,17 @@ const Location = () => {
   return (
     <LocationSection flex column width="100%" id="location">
       <Map
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2904.6227415329054!2d-85.55331042287185!3d43.28027877646906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8818e308742ea39d%3A0x1571453190c166ad!2s17322%20Cedar%20Springs%20Ave%20NE%2C%20Sand%20Lake%2C%20MI%2049343!5e0!3m2!1sen!2sus!4v1689265903330!5m2!1sen!2sus"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2904.159361156258!2d-85.52251842384233!3d43.289984271121604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8818e387fea8748b%3A0x739c33915b5c0014!2s15%20S%207th%20St%2C%20Sand%20Lake%2C%20MI%2049343!5e0!3m2!1sen!2sus!4v1699642277170!5m2!1sen!2sus"
         allowfullscreen=""
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
         title="Google maps link to church address"
       ></Map>
-      <Address target={"_blank"} href="https://goo.gl/maps/8od31JQ9CEokCk6r6">
-        17322 Cedar Springs Ave, Sand Lake, MI 49343
+      <Address
+        target={"_blank"}
+        href="https://maps.app.goo.gl/bZDETgecJoMUTRP87"
+      >
+        15 S. 7th St, Sand Lake, MI 49343
       </Address>
     </LocationSection>
   );
